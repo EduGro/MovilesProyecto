3020e8c1d15d6c9bac4734b7c422eee32fa8e099
EduGro <73360551+EduGro@users.noreply.github.com>
2021-02-24 18:17:07 -0600

    Cambios login y registro

---
+++ lib/utils/constants.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+// home
+const String APP_TITLE = "HOGWARTS";
+// profile
+const String PROFILE_NAME = "Anna Doe";
+const String PROFILE_EMAIL = "anna@doe.com";
+const String PROFILE_PICTURE =
+    "https://dkpp.go.id/wp-content/uploads/2018/10/photo.jpg";
+
+// app
+const _redPrimaryValue = 0xFFD21616;
+const Color PRIMARY_COLOR = const MaterialColor(
+  _redPrimaryValue,
+  const <int, Color>{
+    50: const Color(0xFFD21616),
+    100: const Color(0xFFD21616),
+    200: const Color(0xFFD21616),
+    300: const Color(0xFFD21616),
+    400: const Color(0xFFD21616),
+    500: const Color(_redPrimaryValue),
+    600: const Color(0xFFD21616),
+    700: const Color(0xFFD21616),
+    800: const Color(0xFFD21616),
+    900: const Color(0xFFD21616),
+  },
+);
+
+const _silverSecondaryValue = 0xFFACACAB;
+const Color SECONDARY_COLOR = const MaterialColor(
+  _silverSecondaryValue,
+  const <int, Color>{
+    50: const Color(0xFFACACAB),
+    100: const Color(0xFFACACAB),
+    200: const Color(0xFFACACAB),
+    300: const Color(0xFFACACAB),
+    400: const Color(0xFFACACAB),
+    500: const Color(_silverSecondaryValue),
+    600: const Color(0xFFACACAB),
+    700: const Color(0xFFACACAB),
+    800: const Color(0xFFACACAB),
+    900: const Color(0xFFACACAB),
+  },
+);
+
+//Color(0xFF204254); //Colors.blue; //0xFF204254

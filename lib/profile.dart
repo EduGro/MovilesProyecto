a5a8d9de4bf1003defd14df34ac07ff58dcecef0
EduGro <73360551+EduGro@users.noreply.github.com>
2021-02-25 13:35:25 -0600

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -25,92 +25,116 @@ class Profile extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(24.0),
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Container(
-                  height: 150,
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      PROFILE_PICTURE,
-                    ),
-                    minRadius: 40,
-                    maxRadius: 80,
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Text(
-                  usuario["nombre"],
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline3
-                      .copyWith(color: Colors.black),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Text(
-                  usuario["email"],
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline5
-                      .copyWith(color: Colors.black),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Text(
-                  "Casa",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline5
-                      .copyWith(color: Colors.black),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Varita",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline5
-                      .copyWith(color: Colors.black),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-              ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              "https://lh3.googleusercontent.com/proxy/hZsYtxqQ0fNu7r3aOF89qtw8FAwNCrnZeZBQBy7ko4ZDO68XevD05M5E9di5bXBNmTKEE5aRk3qZ28DZYF8O3sbP9UYJD_qPBnA",
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Expanded(
-                    child: RaisedButton(
-                      child: Text(PROFILE_LOGOUT),
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (context) => Ingreso(title: title)),
-                        );
-                      },
+                  Container(
+                    height: 150,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        PROFILE_PICTURE,
+                      ),
+                      minRadius: 40,
+                      maxRadius: 80,
                     ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    usuario["nombre"],
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3
+                        .copyWith(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    usuario["email"],
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Text(
+                    "Casa:",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        .copyWith(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Gryffindor",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Varita:",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  Image.network(
+                    'https://lh3.googleusercontent.com/proxy/lpxLQWBP6VavVyQuucv8QCY71ASJrWOLLlqg0vB24uaaoAG9s7chwzkCx_FlvvErPYt5VMhj28Ejs77FZpeTN94-VpJB8J_eoSI9QHKUElTHCShQYQ',
+                  ),
+                  SizedBox(
+                    height: 20,
                   ),
                 ],
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Expanded(
+                      child: RaisedButton(
+                        child: Text(PROFILE_LOGOUT),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => Ingreso(title: title)),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
